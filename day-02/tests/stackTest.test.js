474fe3670e1729dcf55bf4c0aec0c9e6db8ec367
Aaron Enyetu <aaronenyetu@gmail.com>
2022-09-20 16:46:07 -0700

    implemented push method and test passing

---
+++ day-02/tests/stackTest.test.js
@@ -0,0 +1,12 @@
+const { Stack } = require('../Stack');
+
+describe('Stack Tests', () => {
+  it('should add an item to an array', () => {
+    const newItemArr = new Stack([]);
+    newItemArr.classPush('fox');
+    newItemArr.classPush('goose');
+    newItemArr.classPush('lizard');
+    expect(newItemArr.readableArr).toEqual(['fox', 'goose', 'lizard']);
+  });
+
+});
