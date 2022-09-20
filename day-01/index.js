db9adc77a22928b4eb374d76442ebd069ea2daf1
Aaron Enyetu <aaronenyetu@gmail.com>
2022-09-20 11:26:06 -0700

    Challenges for PRIM, HOF, and REC solved

--- day-01/index.js
+++ day-01/index.js
@@ -87,6 +87,9 @@ function rootDigit(n) {
   return String(sum).split('').length === 1 ? sum : rootDigit(sum);
 }
 
+
+
+
 module.exports = { reverseSentence, addCapitalToWords, addPunctuation, oddishEvenish, at, anagrams, fizzBuzz, multiples, rootDigit };
  
 
@@ -94,21 +97,3 @@ module.exports = { reverseSentence, addCapitalToWords, addPunctuation, oddishEve
 
 
 
-//----------------------------CODE FOR REVISION-------------------------
-// ADD CAPITAL TO WORDS
-//   return str.charAt(0).toUpperCase() + str.slice(1);
-// }
-
-// function capitalizeWords(str) {
-//   return str.replace(/(^\w{1})|(\s{1}\w{1})/g, (a) => {
-//     return a.toUpperCase();
-//   });
-// }
-// str = str.split('');
-
-// for (let i = 0; i < str.length ; i++) {
-//   str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-// }
-
-// return str.join('');
-// }
